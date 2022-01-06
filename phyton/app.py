245ea4e3b6813cc0c336fcf0dc7631940e07ff3c
Kayevo <kayevo@protonmail.com>
2022-01-06 20:01:07 -0300

    some duplicated characters removed

--- phyton/app.py
+++ phyton/app.py
@@ -4,8 +4,6 @@ stringOfCharacters = ""
  
 listOfCharacters = list(string.ascii_lowercase)
 listOfCharacters += list(string.ascii_uppercase)
-listOfCharacters += list(string.hexdigits)
-listOfCharacters += list(string.octdigits)
 listOfCharacters += list(string.punctuation)
 
 for characterOnList in listOfCharacters:
